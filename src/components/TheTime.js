d8028a0582e35c6be0614127fbb77c80ef92bde0
Paulo Torrealba <paulo@buscalibre.com>
2023-04-07 04:39:08 -0400

    fix interval seconds

--- src/components/TheTime.js
+++ src/components/TheTime.js
@@ -1,10 +1,10 @@
+import { useState, useEffect } from "react";
+
 import css from "../styles/Home.module.css";
 
 function TheTime() {
-	const hours = {
-		hour: String(new Date().getHours()).length <= 1 ? `0${new Date().getHours()}` : new Date().getHours(),
-		minute: String(new Date().getMinutes()).length <= 1 ? `0${new Date().getMinutes()}` : new Date().getMinutes(),
-	};
+	// const [hourInterval, sethourInterval] = useState(`${new Date().getHours()}:${new Date().getMinutes()}:${new Date().getSeconds()}`);
+	const [hourInterval, sethourInterval] = useState("...");
 	const greet = function (param) {
 		if ( param <= 11 ) return "good morning";
 		if ( param <= 18 ) return "good afternoon";
@@ -19,14 +19,28 @@ function TheTime() {
 		return "PM";
 	};
 
+	useEffect(() => {
+		setInterval(() => {
+			const hours = {
+				hour: String(new Date().getHours()).length <= 1 ? `0${new Date().getHours()}` : new Date().getHours(),
+				minute: String(new Date().getMinutes()).length <= 1 ? `0${new Date().getMinutes()}` : new Date().getMinutes(),
+				second: String(new Date().getSeconds()).length <= 1 ? `0${new Date().getSeconds()}` : new Date().getSeconds(),
+			};
+			// console.debug( "asdasd" );
+			const test = `${hours.hour}:${hours.minute}:${hours.second}`;
+			sethourInterval(test);
+		}, 1000);
+	}, []);
+
 	return (
 		<div className={css.hora}>
 			<div className={css.superior}>
-				<img src={image(hours.hour)} alt="sun/moon" />
-				<p>{greet(hours.hour)}, IT&apos;S CURRENTLY</p>
+				<img src={image(new Date().getHours())} alt="sun/moon" />
+				<p>{greet(new Date().getHours())}, IT&apos;S CURRENTLY</p>
 			</div>
 			<div className={css.inferior}>
-				<p>{`${hours.hour}:${hours.minute}`} <span>{amorpm(hours.hour)}</span></p>
+				{/* <p>{`${new Date().getHours()}:${hours.minute}:${hours.second}`} <span>{amorpm(new Date().getHours())}</span></p> */}
+				<p>{hourInterval} <span>{amorpm(new Date().getHours())}</span></p>
 			</div>
 		</div>
 	);
