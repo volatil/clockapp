1062a0122c17864ee946159cc3ae20ad049751a5
Paulo Torrealba <paulo@buscalibre.com>
2023-04-07 02:07:38 -0400

    random image 2

--- src/helpers/helpers.js
+++ src/helpers/helpers.js
@@ -7,6 +7,6 @@ export default function bgrandom() {
 	const max = 6;
 	const min = 1;
 	const numerorandom = Math.floor(Math.random() * (max - min + 1) + min);
-	console.debug( `Numero: ${numerorandom}` );
+	console.debug( `Numero 0207: ${numerorandom}` );
 	return numerorandom;
 }
